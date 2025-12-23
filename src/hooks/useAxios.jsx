c68460d8dc993db02267082dbb525bdcdf223d7b
nayem181 <nayemimranhasan@gmail.com>
2025-12-23 11:19:53 +0600

    secure the key

--- src/hooks/useAxios.jsx
+++ src/hooks/useAxios.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 
 const axiosInstance = axios.create({
-  baseURL: `http://localhost:3000`,
+  baseURL: `${import.meta.env.VITE_API_URL}`,
 });
 
 const useAxios = () => {
